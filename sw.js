34c6090884a8f98fc4afb150c2ac49dcfb70b56b
Star <bintang03aulia@gmail.com>
2026-09-07 22:42:32 +0700

    release: v1.2.2 (build 17) - sync live web assets and APK

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@
  * Service Worker for QR SLOC & MSLTC Generator MTG (Android PWA)
  */
 
-const CACHE_NAME = 'qrsloc-pwa-v23';
+const CACHE_NAME = 'qrsloc-pwa-v24';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './style.css',
-  './app.js?v=50',
+  './app.js?v=51',
   './manifest.json',
   './app-logo.jpg',
   './icon-192.png',
