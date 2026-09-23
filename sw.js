f5295cfa7532f4a0b4e7ba54c3af4bb618ec6a03
tw1nss <tw1nss@users.noreply.github.com>
2026-09-23 11:54:26 +0700

    update v89

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@
  * Service Worker for QR SLOC & MSLTC Generator MTG (Android PWA)
  */
 
-const CACHE_NAME = 'superapp-pwa-v88';
+const CACHE_NAME = 'superapp-pwa-v89';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './style.css?v=88',
-  './app.js?v=88',
+  './style.css?v=89',
+  './app.js?v=89',
   './qrcode.min.js',
   './manifest.json',
   './app-logo.jpg',
